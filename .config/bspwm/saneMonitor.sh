3768a3cfcbb6239655472167d68de9392da63a1c
Nacchofer31 <ignaciofer31@gmail.com>
2020-04-26 13:14:36 +0200

    Updated bspwmrc and config files

--- .config/bspwm/saneMonitor.sh
+++ .config/bspwm/saneMonitor.sh
@@ -9,10 +9,10 @@ elif [ $monnum == 2 ]
 then
 mon1=$(xrandr -q | grep -w connected | awk '{print $1}' |  sed 's/ \+/\n/g' | head -n 1)
 mon2=$(xrandr -q | grep -w connected | awk '{print $1}' |  sed 's/ \+/\n/g' | tail -n 1)
-bspc monitor $mon1 -d 1 2 3 4 5 6 7
-bspc monitor $mon2 -d 8 9 0
+bspc monitor $mon2 -d 1 2 3 4 5 6 7
+bspc monitor $mon1 -d 8 9
 elif [ $monnum == 3 ]
-then 
+then
 mon1=$(xrandr -q | grep -w connected | awk '{print $1}' |  sed 's/ \+/\n/g' | awk NR==1)
 mon2=$(xrandr -q | grep -w connected | awk '{print $1}' |  sed 's/ \+/\n/g' | awk NR==2)
 mon3=$(xrandr -q | grep -w connected | awk '{print $1}' |  sed 's/ \+/\n/g' | awk NR==3)
